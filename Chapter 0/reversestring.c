2858db8e83adcb703518c04c6d95b0d042977bfb
prasad-888 <160460158+prasad-888@users.noreply.github.com>
2025-02-20 13:52:50 +0530

    Add files via upload

---
+++ Chapter 0/reversestring.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int len(String s){
+    for ''
+}
+int main(){
+    char ch = "hello";
+    int left=0;
+    int right=len('hello');
+}
---
+++ Chapter 0/terminating.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char x[6];
+    x[0]='h';
+    x[1]='e';
+    x[2]='l';
+    x[3]='l';
+    x[4]='o';
+    
+    printf("%s",x);
+}
